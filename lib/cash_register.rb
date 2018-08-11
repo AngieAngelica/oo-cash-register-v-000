8d2527867bbf8dbce0999f65befb3edeeb8e98b9
Angie Kassab <anasab12@gmail.com>
2018-08-11 04:16:26 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
   
-  attr_accessor :total, :discount, :items, :price, :quantity :last_transaction
+  attr_accessor :total, :discount, :items, :price, :quantity, :last_transaction
   
   def initialize(employee_discount = 0)
     @total = 0
@@ -12,7 +12,7 @@ class CashRegister
   def add_item(item, price, quantity = 1)
     self.total += (price * quantity)
     quantity.times do
-    @items << item_name
+    @items << item
     end
     self.last_transaction = price * quantity
   end
